49a89cb3515eb7704ba380a5f22a01824f5a8c79
Fanuel Bahta <fanizig8@gmaill.com>
2025-12-24 06:50:40 +0300

    resolving error serviceApi

--- src/Dashboard/ClienDashboard.jsx
+++ src/Dashboard/ClienDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { bookingAPI, barberAPI } from '../../../services/apiService';
+import { bookingAPI, barberAPI } from '../apiServece';
 import DashboardLayout from '../common/DashboardLayout';
 import LoadingSpinner from '../common/LoadingSpinner';
 import { 
