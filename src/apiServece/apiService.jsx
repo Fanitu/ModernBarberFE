4e9dc2466612fce87cc4e6598969799f911201e5
Fanuel Bahta <fanizig8@gmaill.com>
2025-12-26 03:35:45 +0300

    integrating backend url to frontend

--- src/apiServece/apiService.jsx
+++ src/apiServece/apiService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-  baseURL: `${process.env.Backend}/api`,
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
