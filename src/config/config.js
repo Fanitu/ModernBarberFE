158a670526a6df7983ff1326b88e2ec7798884d4
Fanuel Bahta <fanizig8@gmaill.com>
2025-12-26 04:48:36 +0300

    integrating backend url to frontend

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-  backendUrl: process.env.REACT_APP_BACKEND_URL || 'https://barber-shop-nine-kappa.vercel.app/',
+  backendUrl: process.env.REACT_APP_BACKEND_URL || 'https://barber-shop-lilac-ten.vercel.app/',
   isDevelopment: process.env.NODE_ENV === 'development',
   isProduction: process.env.NODE_ENV === 'production',
 };
